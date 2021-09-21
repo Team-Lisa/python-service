4aa9807f524f53a2fb1a026313baf66ca1e08557
matfonseca <mfonseca@fi.uba.ar>
2021-09-21 12:47:19 -0300

    update docs

--- api/routes/helpers.py
+++ api/routes/helpers.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter
 from api import __version__
 
-router = APIRouter()
+router = APIRouter(tags=["Helpers"])
 
 
 @router.get("/")
-async def update_admin():
+async def ping():
     return {"message": "ping"}
 
 
 @router.get("/version")
-async def update_admin():
+async def version():
     return {"version": __version__}
